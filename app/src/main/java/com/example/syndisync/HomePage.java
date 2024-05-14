569c1b3c9b9035c99d45f1aef6209f8f76550144
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-05-14 21:25:21 +0100

    fixed menu bug

--- app/src/main/java/com/example/syndisync/HomePage.java
+++ app/src/main/java/com/example/syndisync/HomePage.java
@@ -1,8 +1,8 @@
 package com.example.syndisync;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class HomePage extends AppCompatActivity {
@@ -10,6 +10,20 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_home_page2);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_home) {
+                // Action when "Home" is selected
+                return true;
+            } else // Action when "Sign Out" is selected
+                if (id == R.id.menu_contact) {
+                // Action when "Contact" is selected
+                return true;
+            } else return id == R.id.menu_signout;
+        });
     }
 }
